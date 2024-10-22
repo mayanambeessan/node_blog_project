399a451ef9d7b453a7d9ddea1a873d6dba844dd2
mayanambeessan <mayamt.calicut@gmail.com>
2024-10-22 21:58:33 +0530

    my first commit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,7 +2,18 @@ const db = require('../config/db');
 const getBlogs = async (req, res) => {
     try {
 
-        const data = await db;
+        const data = await db.query( 'SELECT * FROM blog');
+        if(!data){
+            return res.status(404).send({
+                suceess:false,
+                message:"No data found"
+            })
+        }
+        res.status(200).send({
+            success:true,
+            message:"All Blog data",
+            data : data[0]
+        })
 
     } catch (error) {
         console.log(error)
